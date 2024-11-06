a103f15c5f222b2ed2cd96157bf4b1bf5842f2c6
QuantumXenon <ea994@bath.ac.uk>
2024-11-06 01:17:38 +0000

    Add Block Object so OOP can be used

--- src/main.js
+++ src/main.js
@@ -1,31 +1,48 @@
+// Setup PixiJS application
 const app = new PIXI.Application();
-const imageSize = 32;
-const blocks = [[0, 0, 1], [0, 0, 0]];
+await app.init({background: '#FFFFFF', resizeTo: window});
+document.body.appendChild(app.canvas);
 
-async function setup() {
-    await app.init({background: '#FFFFFF', resizeTo: window});
-    document.body.appendChild(app.canvas);
+// Global Constants
+const IMAGE_SIZE = 32;
+const GRAY = await PIXI.Assets.load('../assets/gray_block_32.png');
+const BLUE = await PIXI.Assets.load('../assets/blue_block_32.png');
+
+// Block class to calculate rendering coordinates and rendering order when created
+class Block extends PIXI.Sprite {
+    rendering_position
+
+    constructor(x, y, z, texture) {
+        super()
+
+        const xCentre = app.screen.width / 2 - IMAGE_SIZE / 2;  // Centre horizontally on screen
+        this.x = (0.50 * x * IMAGE_SIZE) - (0.50 * y * IMAGE_SIZE) + xCentre;
+
+        const zOffset = z * IMAGE_SIZE / 2
+        this.y = (0.25 * x * IMAGE_SIZE) + (0.25 * y * IMAGE_SIZE) + zOffset;
+
+        this.rendering_position = -z * IMAGE_SIZE;  // Calculate position of bottom of sprite
+        this.texture = texture;
+    }
 }
 
+// List of all blocks to be rendered
+const blocks = [new Block(0, 0, 0, GRAY), new Block(0, 0, 1, BLUE)];
+
+// Main logic
 (async () => {
-    await setup();
-    await drawGrid();
+    sortBlocks();
+    drawBlocks();
 })();
 
-async function drawTile(x, y, zOffset) {
-    const xCentre = app.screen.width / 2 - imageSize / 2;
-    const xPos = (0.50 * x * imageSize) - (0.50 * y * imageSize) + xCentre;
-    const yPos = (0.25 * x * imageSize) + (0.25 * y * imageSize);
-    const texture = await PIXI.Assets.load(`../assets/gray_block_${imageSize}.png`);
-    const block = PIXI.Sprite.from(texture);
-
-    block.x = xPos;
-    block.y = yPos + (zOffset * imageSize / 2);
-    return block;
+// Sort blocks to be in suitable rendering order
+function sortBlocks() {
+    blocks.sort((a, b) => a.rendering_position + b.rendering_position);
 }
 
-async function drawGrid() {
-    for (let i = 0; i < blocks.length; i++) {
-        app.stage.addChild(await drawTile(blocks[i][0], blocks[i][1], blocks[i][2]))
+// Render all blocks provided in list
+function drawBlocks() {
+    for (let index = 0; index < blocks.length; index++) {
+        app.stage.addChild(blocks[index])
     }
 }
