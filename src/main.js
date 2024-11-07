31b78459cb0c5fa1ffaf63973b7ddd9965d006ba
QuantumXenon <ea994@bath.ac.uk>
2024-11-07 00:43:42 +0000

    Refactor

--- src/main.js
+++ src/main.js
@@ -18,31 +18,32 @@ class Block extends PIXI.Sprite {
 
     constructor(x, y, z, texture, hasSkyAccess) {
         super({texture: texture});
-        this.hasSkyAccess = hasSkyAccess;
         this.zPos = z;
 
-        const xCentre = app.screen.width / 2 - super.width / 2;  // Centre horizontally on screen
+        const xCentre = app.screen.width / 2 - super.width / 2;  // Centre horizontally on-screen
         super.x = (0.50 * x * super.width) - (0.50 * y * super.height) + xCentre;
-        const yAlign = app.screen.height / 4;  // Align vertically on screen
+        const yAlign = app.screen.height / 4;  // Align vertically on-screen
         const zOffset = this.zPos * super.height / 2;
         super.y = (0.25 * x * super.width) + (0.25 * y * super.height) + yAlign - zOffset;
 
+        this.hasSkyAccess = hasSkyAccess;
         this.rendering_position = this.zPos * super.height;  // Calculate position of bottom of sprite
     }
 }
 
 /* Read blocks from JSON file and make list of Block objects */
 async function createBlocks() {
-    const jsonFile = await PIXI.Assets.load({src: './blocks.json', loader: 'loadJson'})
+    const jsonFile = await PIXI.Assets.load({src: './blocks.json', loader: 'loadJson'});
     const blocks = jsonFile.blocks;
-
     const blockObjects = [];
+
     for (const block of blocks) {
         const coords = {"x": block.x, "y": block.y, "z": block.z + 1};
+        const texture = await PIXI.Assets.load('../assets/' + block.texture)
         if (blocks.some(obj => Object.keys(coords).every(key => obj[key] === coords[key]))) {
-            blockObjects.push(new Block(block.x, block.y, block.z, await PIXI.Assets.load('../assets/' + block.texture), false));
+            blockObjects.push(new Block(block.x, block.y, block.z, texture, false));
         } else {
-            blockObjects.push(new Block(block.x, block.y, block.z, await PIXI.Assets.load('../assets/' + block.texture), true));
+            blockObjects.push(new Block(block.x, block.y, block.z, texture, true));
         }
     }
     return blockObjects;
@@ -52,32 +53,28 @@ const BLOCKS = await createBlocks(); // List of blocks
 
 /* Sort blocks to be in correct rendering order */
 function sortBlocks() {
-    BLOCKS.sort((a, b) => a.rendering_position - b.rendering_position); // Sort blocks by decreasing rendering_position
+    BLOCKS.sort((a, b) => a.rendering_position - b.rendering_position); // Sort descending
 }
 
 /* Render each block in the list */
 function drawBlocks() {
-    for (let index = 0; index < BLOCKS.length; index++) {
-        app.stage.addChild(BLOCKS[index]);
-    }
+    BLOCKS.forEach(block => app.stage.addChild(block));
 }
 
 /* Animate blocks on hover */
 function animateBlocks() {
     BLOCKS.forEach(block => {
-        if (block.hasSkyAccess) {
+        if (block.hasSkyAccess) { // Sky access means that no blocks are above the block
             block.eventMode = 'static';
             block.on('pointerenter', float);
             block.on('pointerleave', descend);
 
             function float() {
-                createjs.Tween.get(block)
-                    .to({y: block.y -= 3}, 1000, createjs.Ease.sineInOut);
+                createjs.Tween.get(block).to({y: block.y -= 3}, 1000, createjs.Ease.sineInOut);
             }
 
             function descend() {
-                createjs.Tween.get(block)
-                    .to({y: block.y += 3}, 1000, createjs.Ease.sineInOut);
+                createjs.Tween.get(block).to({y: block.y += 3}, 1000, createjs.Ease.sineInOut);
             }
         }
     })
